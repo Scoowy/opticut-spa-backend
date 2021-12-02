72e72f647b9abe5e3154d25f6c965b6ab98e6243
Scoowy <gahonajuanjo@gmail.com>
2021-12-02 14:38:11 -0500

    Enhanced log when app run

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import app from "./app";
 
 // Up Server
 app.listen(app.get("port"), () => {
-  console.log(
-    `OptiCut-SPA Backend listening at http://localhost:${app.get("port")}`,
-  );
+  console.log(`OptiCut-SPA Backend listening at port ${app.get("port")}`);
 });
