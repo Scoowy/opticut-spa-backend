50e83c8c2be20c3ff3ad88e170f80a341f4ff9fb
Scoowy <gahonajuanjo@gmail.com>
2021-12-02 19:03:12 -0500

    Fixed CORS

--- src/app.js
+++ src/app.js
@@ -10,7 +10,12 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    optionsSuccessStatus: 200,
+  }),
+);
 app.use(express.json());
 app.use(morgan("dev"));
 
