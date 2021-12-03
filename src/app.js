3e468fe87b59be7969d0c519857f49300b78eb0e
Scoowy <gahonajuanjo@gmail.com>
2021-12-02 19:00:58 -0500

    Allow CORS

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import cors from "cors";
+
 import routes from "./routes/index.routes";
 
 const app = express();
@@ -8,6 +10,7 @@ const app = express();
 app.set("port", process.env.PORT || 3000);
 
 // Middlewares
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
